f2cbf03544cf3e504a8e666b86ca1d1e66c24a88
Vladimir Romanov <vladromweb@gmail.com>
2016-01-13 19:28:28 -0500

    Finalized Layout
    Finalized Layout

--- app.js
+++ app.js
@@ -1,12 +1,2 @@
 // MODULE
 var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
-
-
-
-// SERVICES
-weatherApp.service('cityService', function() {
-    this.city = "New York, NY"
-})
-
-
-
